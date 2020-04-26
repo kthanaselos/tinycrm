4763852d249d49dcf8a4f7d788e7a7e0c2ff7ce9
Konstantinos Thanaselos <kthanaselos@gmail.com>
2020-04-26 13:47:50 +0300

    second commit

--- TinyCRM/TinyCRM/Program.cs
+++ TinyCRM/TinyCRM/Program.cs
@@ -6,7 +6,63 @@ namespace TinyCRM
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Dose afm");
+
+            if (IsValidAfm(Console.ReadLine()))
+            {
+                Console.WriteLine("valid afm");
+            }
+            else
+            {
+                Console.WriteLine("NOT valid afm");
+            }
+        }
+
+        public static bool IsValidAfm(string afm)
+        {
+            if (string.IsNullOrWhiteSpace(afm))
+            {
+                return false;
+            }
+
+            afm = afm.Trim();
+
+            if (int.TryParse(afm, out int result) && afm.Length == 9)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsAdult(int age)
+        {
+            if (age >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return false;
+            }
+            email = email.Trim();
+
+            if (email.Contains("@") && email.EndsWith(".com"))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
